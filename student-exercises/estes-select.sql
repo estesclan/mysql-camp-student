242874139555105c844050a4f90c932b062aee14
Peter Estes <peternigelestes@gmail.com>
2018-06-28 13:13:03 -0400

    exercise 3

--- student-exercises/estes-select.sql
+++ student-exercises/estes-select.sql
@@ -1,2 +1,4 @@
-select ID, name, genre
-from band;
+select * from individual where lastName="Jennings";
+select ID, lastName from individual where deceasedDate is not null;
+select ID, lastName, birthDate from individual where year (BirthDate)>1940;
+select * from individual where ID in (1,3,5,7,9);
