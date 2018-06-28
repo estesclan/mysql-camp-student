7f5d9c3b8320b0cbb06632f380dde910f33edd51
Peter Estes <peternigelestes@gmail.com>
2018-06-28 15:46:34 -0400

    exercise 5

--- student-exercises/estes-select.sql
+++ student-exercises/estes-select.sql
@@ -2,3 +2,9 @@ select * from individual where lastName="Jennings";
 select ID, lastName from individual where deceasedDate is not null;
 select ID, lastName, birthDate from individual where year (BirthDate)>1940;
 select * from individual where ID in (1,3,5,7,9);
+
+select * from band where name like  'The%';
+select * from band where name like '%Stones';
+select * from individual where firstName like '__ck';
+select * from band where name like '% and %';
+select * from individual where firstName like '_im';
