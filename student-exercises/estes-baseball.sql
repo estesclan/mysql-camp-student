e138f7d2799ea89cb1d37c8909b5794c922d1735
Peter Estes <peternigelestes@gmail.com>
2018-07-01 16:07:02 -0400

    even more query examples

--- student-exercises/estes-baseball.sql
+++ student-exercises/estes-baseball.sql
@@ -52,8 +52,8 @@ JOIN roster ON team.ID = roster.playerID;
 SELECT 
 team.teamName AS team 
 FROM team 
-LEFT JOIN roster ON team.ID = roster.teamID WHERE roster.playerID IS NULL; 
--- ORDER BY team.teamName DESC; 
+LEFT JOIN roster ON team.ID = roster.teamID WHERE roster.playerID IS NULL
+ORDER BY team.teamName DESC; 
 
 SELECT count(*) FROM team;
 
@@ -78,6 +78,43 @@ roster. *
 team.teamName as 'No Players In Roster'
 FROM team 
 LEFT JOIN roster ON team.ID = roster.playerID where roster.playerID is null;
+
+SELECT birthplaceCity, 
+count(bats) as 'bats R' from player where bats = 'R' GROUP BY birthplaceCity desc;
+
+SELECT
+max(batting.hits),
+player.lastName
+from batting join player ON player.ID = batting.playerID;
+
+
+SELECT birthplaceCountry, COUNT(*)as 'playerCount'
+FROM player
+GROUP BY birthplaceCountry
+ORDER BY birthplaceCountry;
+
+SELECT divisionName, COUNT(*)as 'teamCount'
+FROM team
+GROUP BY divisionName;
+
+SELECT lastName, birthplaceCountry
+FROM player
+ORDER BY birthplaceCountry;
+
+SELECT count(birthplaceCountry)vbattingleaders
+FROM player
+WHERE birthplaceCountry = 'USA'
+
+SELECT
+team.*,
+batting.*
+FROM team
+INNER JOIN batting on team.ID = batting.teamID
+WHERE batting.battingAvg > 320
+ORDER BY batting.battingAvg;
+
+
+
 
 
 
